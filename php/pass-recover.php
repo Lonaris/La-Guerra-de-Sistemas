e21b8eb9c5f4dc6c75cd063c9802653fb78741c6
Lonaris <juanjorc98@gmail.com>
2016-09-09 13:05:49 -0300

    Actualización de las páginas
    Un pequeño cambio en Home.html para que no me muestre la contraseña y
    dos nuevas páginas para ingresar y registrarme.

---
+++ php/pass-recover.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset = "UTF-8">
+		<link rel = "stylesheet" type = "text/css" href = "styles.css">
+		<title>La guerra de Sistemas - Login</title> <!--Estatico, obvio-->
+
+	</head>
+	<body onunload = 'surrender()'>
+		<header>
+			<!--Estatico-->
+			<div class = "Title-Block">
+				<span class = "Image"><img id = logo src = "images/ISFT-50x50.png"></span>
+				<h1>La guerra de sistemas</h1>
+			</div>
+
+	</body>
+</html>
---
+++ php/register.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset = "UTF-8">
+		<link rel = "stylesheet" type = "text/css" href = "../styles/styles-all-center.css">
+		<title>La guerra de Sistemas - Login</title> <!--Estatico, obvio-->
+
+	</head>
+	<body>
+		<header>
+			<!--Estatico-->
+			<div class = "Title-Block">
+				<span class = "Image"><img id = logo src = "../images/ISFT-500x500.png"></span>
+				<h1>La guerra de sistemas</h1>
+			</div>
+		</header>
+		<main>
+			<h1>Bienvenido</h1>
+			<div class="Input-Wrapper">
+				<input placeholder="Nombre de usuario" id="Username"></input>
+				<input type="password" placeholder="Contraseña" id="Password"></input>
+			</div>
+			<input type="submit" value="Ingresar"></input>
+		</main>
+	</body>
+</html>
